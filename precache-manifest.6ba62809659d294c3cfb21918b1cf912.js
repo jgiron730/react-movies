fcf3b1234b647c7db4a0acf3348b4a0300bd2918
jgiron730 <badum7@gmail.com>
2020-05-30 10:55:26 -0600

    Updates

---
+++ precache-manifest.6ba62809659d294c3cfb21918b1cf912.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6870f219862c9e0a3791caaaf19d633c",
+    "url": "/react-movies/index.html"
+  },
+  {
+    "revision": "3914eedafc4f1a67cd3b",
+    "url": "/react-movies/static/css/main.dbf73fb6.chunk.css"
+  },
+  {
+    "revision": "4b9933f5520a9c76098a",
+    "url": "/react-movies/static/js/2.97ba5ce7.chunk.js"
+  },
+  {
+    "revision": "3914eedafc4f1a67cd3b",
+    "url": "/react-movies/static/js/main.108b3659.chunk.js"
+  },
+  {
+    "revision": "5aabf462209044c73a79",
+    "url": "/react-movies/static/js/runtime-main.8d39c7cf.js"
+  },
+  {
+    "revision": "1f066db28737338c1eb52c705637845e",
+    "url": "/react-movies/static/media/noimage.1f066db2.jpg"
+  }
+]);
